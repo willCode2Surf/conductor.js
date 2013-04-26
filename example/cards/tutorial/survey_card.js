18c8780f0f9bddf5dddbe1979585033ccee4f60b
David J. Hamilton <davidjh@hjdivad.com>
2013-04-26 07:16:10 -0700

    Tutorial: Load dependencies in cards.

--- example/cards/tutorial/survey_card.js
+++ example/cards/tutorial/survey_card.js
@@ -1,11 +1,21 @@
 /*global Handlebars*/
 
+Conductor.require('/example/libs/jquery-1.9.1.js');
+Conductor.require('/example/libs/handlebars-1.0.0-rc.3.js');
+
+var template = '{{message}}';
+
 Conductor.card({
+
   activate: function( data ) {
-    this.message = "hello world";
+    this.compileTemplates();
+  },
+
+  compileTemplates: function() {
+    template = Handlebars.compile(template);
   },
 
   render: function () {
-    document.body.innerHTML = this.message;
+    $('body').html(template({ message: "Hello again, world!" }));
   }
 });
